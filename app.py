5603b8f406cfb31883bb46d236b90ffe5e0c719f
VasundharaShivankar <vasundharashivankar179@gmail.com>
2025-11-22 22:05:45 +0530

    flask deployment in progress

--- app.py
+++ app.py
@@ -431,5 +431,6 @@ def predict():
     else:
         return jsonify({'error': 'No file uploaded'}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
+
